e92a9016427bae96538c91dc0cc78d4587c20752
Alex Marina <alex@192-168-0-103.rdsnet.ro>
2020-11-26 15:46:38 +0200

    build

--- server.js
+++ server.js
@@ -2,31 +2,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 if ( process.env.NODE_ENV !== 'production' ) require('dotenv').config();
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
-var server = express();
+var app = express();
+const port = process.env.PORT || 5000;
 
-server.use(logger('dev'));
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
-server.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
 
 if ( process.env.NODE_ENV === 'production' ) {
-  server.use(express.static(path.join(__dirname, 'client/build')));
-  server.get('*', (req, res) => {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   });
 }
 
-server.get('*', (res, req) => {
+app.get('*', (res, req) => {
   res.status(200).json({port: process.env.PORT});
 });
 
-// server.use('/', indexRouter);
-// server.use('/users', usersRouter);
+app.listen(port, error => {
+  if ( error ) throw error;
+  console.log('Server running on port ' + port);
+});
 
-module.exports = server;
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
