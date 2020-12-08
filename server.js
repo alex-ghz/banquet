8807694c67ac07545914049b120acfada6d4ee94
Alex Marina <alex@192-168-0-103.rdsnet.ro>
2020-12-09 00:41:28 +0200

    remove whitespace

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ if (!databaseUri) {
 // Database
 Parse = require('parse/node');
 Parse.serverURL = process.env.SERVER_URL || 'http://localhost:5000/parse'; // This is your Server URL
-Parse.initialize(
-    process.env.APP_ID || 'app');
+Parse.initialize(process.env.APP_ID || 'app');
 
 const parse = new ParseServer({
 	databaseURI: databaseUri || 'mongodb+srv://test:test@cluster0.mtjj2.mongodb.net/Chef?retryWrites=true&w=majority',
