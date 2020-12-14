44f7d220c153b26d88760fd344eb8d80e111dadc
Alex Marina <alex@192-168-0-103.rdsnet.ro>
2020-12-15 01:50:54 +0200

    remove bug

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const { userGenerator } = require('../utils/user');
-
 router.post('/register', async (req, res) => {
 	let { email, name, phoneNo, password } = req.body;
 
