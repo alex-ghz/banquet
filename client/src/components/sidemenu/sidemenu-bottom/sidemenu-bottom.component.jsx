48cc0d94c3b196662037a19b74705208a1c1fb9b
vladalar <vladpopa63@gmail.com>
2021-01-15 15:34:16 +0200

    Added log out button in sidemenu

--- client/src/components/sidemenu/sidemenu-bottom/sidemenu-bottom.component.jsx
+++ client/src/components/sidemenu/sidemenu-bottom/sidemenu-bottom.component.jsx
@@ -7,6 +7,9 @@ const SideMenuBottom = () => (
 		<div className="order_toggle_text medium_sofia hidden_text">
 			Accepting orders
 		</div>
+		<div className="log-out-side sb_sofia">
+			Log out
+		</div>
 		<label className="switch">
 			<input type="checkbox"/>
 			<span className="slider slider_sidemen round"/>
