db33b06ef4d06c98bab8d43020bd446d0608faad
vladalar <vladpopa63@gmail.com>
2021-01-15 15:50:17 +0200

    test

--- client/src/components/sidemenu/sidemenu-bottom/sidemenu-bottom.component.jsx
+++ client/src/components/sidemenu/sidemenu-bottom/sidemenu-bottom.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './sidemenu-bottom.styles.scss';
 
 const SideMenuBottom = () => (
-	[ <div className="order_toggle">
+	<div className="order_toggle">
 		<div className="order_toggle_text medium_sofia hidden_text">
 			Accepting orders
 		</div>
@@ -11,10 +11,8 @@ const SideMenuBottom = () => (
 			<input type="checkbox"/>
 			<span className="slider slider_sidemen round"/>
 		</label>
-	</div> ]
-	[ <div className="log-out-side sb_sofia">
-		Log out
-	</div> ]
+	</div>
+	
 );
 
 export default SideMenuBottom;
