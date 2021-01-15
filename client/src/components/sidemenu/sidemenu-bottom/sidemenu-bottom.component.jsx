726d4d43066a3554f6f2cc5a6436617bdfda74e8
vladalar <vladpopa63@gmail.com>
2021-01-15 15:44:05 +0200

    small fix resolved

--- client/src/components/sidemenu/sidemenu-bottom/sidemenu-bottom.component.jsx
+++ client/src/components/sidemenu/sidemenu-bottom/sidemenu-bottom.component.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 import './sidemenu-bottom.styles.scss';
 
 const SideMenuBottom = () => (
-	<div className="order_toggle">
+	[ <div className="order_toggle">
 		<div className="order_toggle_text medium_sofia hidden_text">
 			Accepting orders
-		</div>
-		<div className="log-out-side sb_sofia">
-			Log out
 		</div>
 		<label className="switch">
 			<input type="checkbox"/>
 			<span className="slider slider_sidemen round"/>
 		</label>
-	</div>
+	</div> ]
+	[ <div className="log-out-side sb_sofia">
+		Log out
+	</div> ]
 );
 
 export default SideMenuBottom;
