f6d136661d5c0a71d205359120834499dd454cd3
Alex <marinagalexandru@gmail.com>
2021-01-16 16:25:50 +0200

    see server env

--- routes/profile.js
+++ routes/profile.js
@@ -31,52 +31,6 @@ router.post('/update', async (req, res) => {
 			 });
 });
 
-router.post('/saveData', (req, res) => {
-	const { key, chefId } = req.body;
-
-	if ( key === 'profileImg' ) {
-		const file = req.files.file;
-		const userFileName = file.name;
-
-		const data = Array.from(Buffer.from(file.data, 'binary'));
-		const parseFile = new Parse.File(userFileName, data);
-
-		parseFile.save()
-				 .then(result => {
-					 const Chef = Parse.Object.extend("Chef");
-					 const queryChef = new Parse.Query(Chef);
-
-					 queryChef.get(chefId)
-							  .then(chef => {
-								  chef.set("profilePhotoURL", result.url());
-
-								  chef.save()
-									  .then(chef => {
-										  res.json({ photoUrl: result.url() });
-									  })
-							  });
-				 });
-	}
-
-	if ( key === 'description' ) {
-		const { data } = req.body;
-
-		const Chef = Parse.Object.extend("Chef");
-		const queryChef = new Parse.Query(Chef);
-
-		queryChef.get(chefId)
-				 .then(chef => {
-					 chef.set(key, data);
-
-					 chef.save()
-						 .then(chef => {
-							 res.json({ done: true });
-						 })
-				 })
-	}
-
-});
-
 function saveChefPhoto(file) {
 	return new Promise(resolve => {
 		const userFileName = file.name;
@@ -92,7 +46,6 @@ function saveChefPhoto(file) {
 }
 
 function saveChefDetails(chef, body, cb) {
-	console.log(body);
 	const {
 		description,
 		delivery,
--- server.js
+++ server.js
@@ -85,7 +85,7 @@ const dashboard = new ParseDashboard({
 }, options);
 
 app.use('/dashboard', dashboard);
-
+console.log(process.env.NODE_ENV)
 if ( process.env.NODE_ENV === 'production' ) {
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', (req, res) => {
