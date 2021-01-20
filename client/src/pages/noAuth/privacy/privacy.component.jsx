01055eb4eb0c9ecad19cd8545d2c4373be0f0148
vladalar <vladpopa63@gmail.com>
2021-01-20 20:15:33 +0200

    Modified privacy page

--- client/src/pages/noAuth/privacy/privacy.component.jsx
+++ client/src/pages/noAuth/privacy/privacy.component.jsx
@@ -14,11 +14,10 @@ const Privacy = () => (
 					I. Introduction
 				</div>
 				<div className="story_page_text story_page_section_content">
-					When you use Banquet, you trust us with your personal data. We’re committed to keeping that trust. That starts with helping you understand our privacy practices.
-					This notice describes the personal data we collect, how it’s used and shared, and your choices regarding this data. We recommend that you read this along with our privacy overview,
-					which highlights key points about our privacy practices.<br/>
-					Last modified: January 14, 2021<br/>
-					Effective date: January 14, 2021<br/>
+					<p className="txt-par">When you use Banquet, you trust us with your personal data. We’re committed to keeping that trust. That starts with helping you understand our privacy practices.</p>
+					<p className="txt-par">This notice describes the personal data we collect, how it’s used and shared, and your choices regarding this data. We recommend that you read this along with our privacy overview, which highlights key points about our privacy practices.</p>
+					<p className="txt-par">Last modified: January 14, 2021</p>
+					<p className="txt-par">Effective date: January 14, 2021</p>
 				</div>
 			</div>
 			<div className="story_page_section">
@@ -26,18 +25,20 @@ const Privacy = () => (
 					II. Overview
 				</div>
 				<div className="story_page_text story_page_section_content">
-					A. Scope
+					<p className="sub-title-privacy">A. Scope</p>
 					This notice applies to users of Banquet’s services anywhere in the world, including users of Banquet’s apps, websites, features, or other services.<br/>
 					This notice describes how Banquet and its affiliates collect and use personal data. This notice applies to all users of our apps, websites, features, or other services anywhere in
 					the world, unless covered by a separate privacy notice. This notice specifically applies to:<br/>
-					Riders: individuals who request or receive transportation, including those who receive transportation requested by another individual<br/>
-					Drivers: individuals who provide transportation to Riders individually or through partner transportation companies<br/>
-					Delivery recipients: individuals who request or receive food, or other products and services<br/>
-					Delivery persons: individuals who provide delivery or other services<br/><br/>
-					This notice also governs Banquet’s other collections of personal data in connection with Banquet’s services. For example, we may collect the contact information of individuals who
+					<ul className="bullet-lst">
+						<li className="bullet-elm">Riders: individuals who request or receive transportation, including those who receive transportation requested by another individual</li>
+						<li className="bullet-elm">Drivers: individuals who provide transportation to Riders individually or through partner transportation companies</li>
+						<li className="bullet-elm">Delivery recipients: individuals who request or receive food, or other products and services</li>
+						<li className="bullet-elm">Delivery persons: individuals who provide delivery or other services</li>
+					</ul>
+					<p className="txt-par">This notice also governs Banquet’s other collections of personal data in connection with Banquet’s services. For example, we may collect the contact information of individuals who
 					use accounts owned by Banquet for Business customers or of owners or employees of Banquet Eats chef partners. We may also collect the personal data of those who start but do not
 					complete applications to be drivers or delivery persons, those who rent bicycles, scooters, or other light electrical devices offered by other companies through an Banquet app, or
-					in connection with our mapping technology and features.<br/>
+					in connection with our mapping technology and features.</p>
 					All those subject to this notice are referred to as “users” in this notice.<br/>
 					For Guest Users: The personal data of those who order or receive trips or deliveries via partner websites or apps (such as when ordering from a chef or grocery store), or arranged
 					by other account owners (collectively “Guest Users”) may be used for purposes described in this notice, and may be associated with, and accessible by, the owner of that account.
