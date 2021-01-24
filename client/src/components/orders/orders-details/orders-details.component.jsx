8a684a3341ad438bdc67dc49472382d04b617734
vladalar <vladpopa63@gmail.com>
2021-01-24 21:12:50 +0200

    orders items reordered

--- client/src/components/orders/orders-details/orders-details.component.jsx
+++ client/src/components/orders/orders-details/orders-details.component.jsx
@@ -266,9 +266,9 @@ class OrdersDetails extends React.Component {
 								<div className="order_panel_detail">
 									<div className="order_panel_detail_left">
 										<div className="order_panel_category sb_sofia">MAINS</div>
-										<p className="product_qty medium_sofia">0 x</p>
 									</div>
 									<div className="order_panel_detail_right medium_sofia">
+										<p className="product_qty medium_sofia">0 x</p>
 										<div className="order_panel_product_name">Dish Name</div>
 										<div className="order_panel_product_price">£0.00</div>
 									</div>
